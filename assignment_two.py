8aff0ee1c7d68701a73a12e58d1285df05c78afc
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2024-02-29 10:31:39 +0300

    Add files via upload

---
+++ assignment_two.py
@@ -0,0 +1,22 @@
+mylist = []
+
+mylist.append(10)
+mylist.append(20)
+mylist.append(30)
+mylist.append(40)
+
+
+mylist[1] = 15
+
+mylist.extend([50, 60, 70])
+
+mylist.pop()
+
+mylist.sort()
+
+
+print(mylist[2])
+
+
+
+
