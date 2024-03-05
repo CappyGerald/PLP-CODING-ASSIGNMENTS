a072f25341b531b480d6bb54827d227ae706b981
Gerald Odhiambo <geraldodhiambo67@gmail.com>
2024-03-05 18:22:36 +0300

    Added data_types.dart file

---
+++ data_types_assignment.dart
@@ -0,0 +1,63 @@
+//practing data types in dart
+
+//1: Numbers-- double and int
+
+
+void main(){
+
+
+   var num = 10; // int
+   var num2 = 5.0; // double
+   print(num + num2);
+
+   print(num + num2);
+
+// 2: strings
+
+String name = 'James';
+print(name);
+
+
+// maps- associate keys with values
+
+var food = {'meat': 'goat', 'milk': 'cow'};
+print("I went to the city.");
+print("and bought $food");
+
+
+
+// 4:list
+
+var assets = ['cash', 'car', 'house'];
+print(assets);
+
+print(assets[1]);
+
+
+// 5: sets
+
+var nums = {
+  'cookies', 'smartphone', 'trailer'
+};
+
+print(nums);
+
+// Boaleans
+
+print("Do you have a name?");
+bool another_name = true;
+print(name);
+
+// Runes-used to represent 
+//Unicode characters
+
+var emojiString = "I love ❤";
+
+// printing UTF-16 code units of a string
+print('UTF-16 code units:${emojiString.codeUnits}');
+
+//symbols-for reflecting metadata on a type e.g library/class
+
+}
+
+
