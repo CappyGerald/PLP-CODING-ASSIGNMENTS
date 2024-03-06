bc36ca091d5eff7a9cd8b95cad7070c9748b076e
Gerald Odhiambo <geraldodhiambo67@gmail.com>
2024-03-06 14:06:08 +0300

    Added a student performance rater

---
+++ student_rater.py
@@ -0,0 +1,20 @@
+def rate_student_performance(student_score):
+    if student_score >= 80:
+        print("Congratulation!passed with distinction!")
+    elif student_score in range(60, 71):
+        print("passed with credit!")
+    elif student_score in range(40, 51):
+         print("Fair. You tried")
+    else:
+          print("failed")
+          
+          
+def main(): 
+    student_score = int(input("Enter student score:"))
+    rate_student_performance(student_score)
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
