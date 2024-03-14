9b6dea5fce9167e9e9e581d589476663719c8bbe
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2024-03-14 16:02:55 +0300

    Update assignment_two.py

--- assignment_two.py
+++ assignment_two.py
@@ -1,12 +1,9 @@
 mylist = []
 
-mylist.append(10)
-mylist.append(20)
-mylist.append(30)
-mylist.append(40)
+mylist.extend([10,20,30,40])
 
 
-mylist[1] = 15
+mylist.insert(1,15)
 
 mylist.extend([50, 60, 70])
 
